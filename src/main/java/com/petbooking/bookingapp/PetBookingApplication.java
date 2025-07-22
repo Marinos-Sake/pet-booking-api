9c45c5d1a2613a7251b63c939eb7e8ce8bc44a74
Marinos.Sake <protutor13@hotmail.com>
2025-07-22 19:56:37 +0300

    Initial commit

---
+++ src/main/java/com/petbooking/bookingapp/PetBookingApplication.java
@@ -0,0 +1,13 @@
+package com.petbooking.bookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petbooking/bookingapp/PetBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petbooking.bookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

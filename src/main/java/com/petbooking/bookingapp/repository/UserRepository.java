83c730755c6a1b709172f648e8fc26090dd47c18
Marinos.Sake <protutor13@hotmail.com>
2025-07-25 01:12:05 +0300

    feat: add UserRepository interface for user entity

---
+++ src/main/java/com/petbooking/bookingapp/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.petbooking.bookingapp.repository;
+
+import com.petbooking.bookingapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}

3c26db72248a68a493222f82c2a4f1bdd832ba26
Marinos.Sake <protutor13@hotmail.com>
2025-07-24 20:20:59 +0300

    chore: enable JPA auditing with @EnableJpaAuditing annotation

--- src/main/java/com/petbooking/bookingapp/PetBookingApplication.java
+++ src/main/java/com/petbooking/bookingapp/PetBookingApplication.java
@@ -2,7 +2,10 @@ package com.petbooking.bookingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class PetBookingApplication {
 

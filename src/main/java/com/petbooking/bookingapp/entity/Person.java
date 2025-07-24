103b63fa71199c2036045453d3443f34f0086b52
Marinos.Sake <protutor13@hotmail.com>
2025-07-24 20:24:57 +0300

    feat: add Person entity for storing identification details

---
+++ src/main/java/com/petbooking/bookingapp/entity/Person.java
@@ -0,0 +1,54 @@
+package com.petbooking.bookingapp.entity;
+
+import com.petbooking.bookingapp.core.enums.Gender;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name= "persons")
+public class Person  extends AbstractEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String surname;
+
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Column(name = "place_of_birth", nullable = false, length = 100)
+    private String placeOfBirth;
+
+    @Column(name= "father_name", nullable = false, length = 100)
+    private String fatherName;
+
+    @Column(name = "identity_number", nullable = false, unique = true, length = 9)
+    private String identityNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "identity_number_file_id")
+    private Attachment identityNumberFile;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10)
+    private Gender gender;
+
+    @OneToOne(mappedBy = "person")
+    private User user;
+
+
+}

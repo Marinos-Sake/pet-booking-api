fd799716d11c6afc4d06510581a5dfec9af227dd
Marinos.Sake <protutor13@hotmail.com>
2025-07-29 21:27:25 +0300

    feat: ResponseMessageDTO for Exceptions

---
+++ src/main/java/com/petbooking/bookingapp/dto/ResponseMessageDTO.java
@@ -0,0 +1,14 @@
+package com.petbooking.bookingapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ResponseMessageDTO {
+    private final LocalDateTime timestamp;
+    private final String code;
+    private final String message;
+}

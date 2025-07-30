ccf946165c63e67bfc74ccba7fb1c5b9ddda9c00
Marinos.Sake <protutor13@hotmail.com>
2025-07-30 20:20:55 +0300

    feat: add existsByUsername method with Spring Data auto-implementation

--- src/main/java/com/petbooking/bookingapp/repository/UserRepository.java
+++ src/main/java/com/petbooking/bookingapp/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.petbooking.bookingapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByUsername(String username);
 }

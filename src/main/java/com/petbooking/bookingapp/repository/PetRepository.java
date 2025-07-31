e085f82541bdeff90b4a42bf3035c37e71941fb3
Marinos.Sake <protutor13@hotmail.com>
2025-07-31 21:33:47 +0300

    feat: add findAllByOwner method to PetRepository for querying pets by person

--- src/main/java/com/petbooking/bookingapp/repository/PetRepository.java
+++ src/main/java/com/petbooking/bookingapp/repository/PetRepository.java
@@ -1,7 +1,12 @@
 package com.petbooking.bookingapp.repository;
 
+import com.petbooking.bookingapp.entity.Person;
 import com.petbooking.bookingapp.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PetRepository extends JpaRepository<Pet, Long> {
+
+    List<Pet> findAllByOwner(Person owner);
 }

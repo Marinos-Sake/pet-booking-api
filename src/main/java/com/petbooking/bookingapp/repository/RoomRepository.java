410a9354772db8a1881a4f51ce271d3b17d09a5c
Marinos.Sake <protutor13@hotmail.com>
2025-08-02 21:47:17 +0300

    feat(room-repository): add custom query to update room availability

--- src/main/java/com/petbooking/bookingapp/repository/RoomRepository.java
+++ src/main/java/com/petbooking/bookingapp/repository/RoomRepository.java
@@ -2,6 +2,14 @@ package com.petbooking.bookingapp.repository;
 
 import com.petbooking.bookingapp.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    @Modifying
+    @Query("UPDATE Room r SET r.isAvailable = :available WHERE r.id = :id")
+    void updateAvailability(@Param("id") Long id, @Param("available") boolean available);
+
 }

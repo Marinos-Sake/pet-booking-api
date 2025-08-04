8eed033be83e04832e0c042afddc901c3440fe50
Marinos.Sake <protutor13@hotmail.com>
2025-08-04 22:33:36 +0300

    style(exception): fix minor syntax spacing in AppAccessDeniedException

--- src/main/java/com/petbooking/bookingapp/core/exception/AppAccessDeniedException.java
+++ src/main/java/com/petbooking/bookingapp/core/exception/AppAccessDeniedException.java
@@ -4,7 +4,7 @@ package com.petbooking.bookingapp.core.exception;
 public class AppAccessDeniedException extends AppGenericException {
     private static final String DEFAULT_CODE = "AccessDenied";
 
-  public AppAccessDeniedException(String code, String message) {
-    super(code + DEFAULT_CODE, message);
-  }
+    public AppAccessDeniedException(String code, String message) {
+        super(code + DEFAULT_CODE, message);
+    }
 }

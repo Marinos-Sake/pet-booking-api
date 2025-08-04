cac7cc9b5e09fd41c92d3a897036f2e52d7ba229
Marinos.Sake <protutor13@hotmail.com>
2025-08-04 22:25:45 +0300

    feat(review): add repository methods to fetch reviews by booking and by user

--- src/main/java/com/petbooking/bookingapp/repository/ReviewRepository.java
+++ src/main/java/com/petbooking/bookingapp/repository/ReviewRepository.java
@@ -3,5 +3,12 @@ package com.petbooking.bookingapp.repository;
 import com.petbooking.bookingapp.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Optional<Review> findByBookingId(Long bookingId);
+
+    List<Review> findByUserId(Long userId);
 }

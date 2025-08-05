f9137694e366362da54238848e88721dd8e737a8
Marinos.Sake <protutor13@hotmail.com>
2025-08-05 23:34:19 +0300

    feat(payment): add custom repository method to fetch user payments

--- src/main/java/com/petbooking/bookingapp/repository/PaymentRepository.java
+++ src/main/java/com/petbooking/bookingapp/repository/PaymentRepository.java
@@ -3,5 +3,10 @@ package com.petbooking.bookingapp.repository;
 import com.petbooking.bookingapp.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    List<Payment> findAllByBooking_User_Id(Long userId);
+
 }

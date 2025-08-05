5663bcf2a3b25437b077a24eca575d666ef5bdc8
Marinos.Sake <protutor13@hotmail.com>
2025-08-05 23:33:55 +0300

    feat(attachment): add read-only DTO for attachment response

---
+++ src/main/java/com/petbooking/bookingapp/dto/AttachmentReadOnlyDTO.java
@@ -0,0 +1,21 @@
+package com.petbooking.bookingapp.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AttachmentReadOnlyDTO {
+
+    private Long id;
+
+    private String fileName;
+
+    private String ContentType;
+
+    private String extension;
+
+}

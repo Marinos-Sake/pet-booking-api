d0a58cf4b38df9b2c7669b4f49f5bc345fa4bfa8
Marinos.Sake <protutor13@hotmail.com>
2025-08-09 03:51:54 +0300

    feat(pet): add repository method to find pet by id and owner id

--- src/main/java/com/petbooking/bookingapp/repository/PetRepository.java
+++ src/main/java/com/petbooking/bookingapp/repository/PetRepository.java
@@ -5,8 +5,11 @@ import com.petbooking.bookingapp.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
 
     List<Pet> findAllByOwner(Person owner);
+
+    Optional<Pet> findByIdAndOwnerId(Long id, Long ownerId);
 }

0b622443a7c5a26fe0bb081ab52aaa443db8020e
Marinos.Sake <protutor13@hotmail.com>
2025-08-12 19:30:00 +0300

    feat(auth): add LoginResponseDTO for structured login responses

---
+++ src/main/java/com/petbooking/bookingapp/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.petbooking.bookingapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LoginResponseDTO {
+
+    private String token;
+}

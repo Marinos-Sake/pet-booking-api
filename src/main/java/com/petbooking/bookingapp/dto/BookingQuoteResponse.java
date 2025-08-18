8a7ee13eb3115a99715c024ad61b9f73c1a3bdf9
Marinos.Sake <protutor13@hotmail.com>
2025-08-18 15:50:15 +0300

    feat(booking): add BookingQuoteResponse DTO

---
+++ src/main/java/com/petbooking/bookingapp/dto/BookingQuoteResponse.java
@@ -0,0 +1,14 @@
+package com.petbooking.bookingapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+public class BookingQuoteResponse {
+    private long nights;
+    private BigDecimal pricePerNight;
+    private BigDecimal totalPrice;
+}

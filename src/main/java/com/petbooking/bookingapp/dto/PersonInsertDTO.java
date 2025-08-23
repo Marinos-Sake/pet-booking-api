74f03943b396c634a229e87d54310d4512cfee8c
Marinos.Sake <protutor13@hotmail.com>
2025-08-23 04:01:22 +0300

    fix(dto): enforce past or present validation on PersonInsertDTO dateOfBirth

--- src/main/java/com/petbooking/bookingapp/dto/PersonInsertDTO.java
+++ src/main/java/com/petbooking/bookingapp/dto/PersonInsertDTO.java
@@ -3,6 +3,7 @@ package com.petbooking.bookingapp.dto;
 import com.petbooking.bookingapp.core.enums.Gender;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,6 +22,7 @@ public class PersonInsertDTO {
     @NotBlank(message = "Surname is required")
     private String surname;
 
+    @PastOrPresent(message = "The date of birth cannot be in the future.")
     @NotNull(message = "Date of Birth is required")
     private LocalDate dateOfBirth;
 

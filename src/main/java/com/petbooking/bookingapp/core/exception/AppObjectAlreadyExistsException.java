fccc498479186ff71f86bb81e686b57e7364ef99
Marinos.Sake <protutor13@hotmail.com>
2025-08-28 19:27:32 +0300

    refactor(exception): standardize DEFAULT_CODE naming to ALREADY_EXISTS

--- src/main/java/com/petbooking/bookingapp/core/exception/AppObjectAlreadyExistsException.java
+++ src/main/java/com/petbooking/bookingapp/core/exception/AppObjectAlreadyExistsException.java
@@ -3,7 +3,7 @@ package com.petbooking.bookingapp.core.exception;
 
 public class AppObjectAlreadyExistsException extends AppGenericException {
 
-    private static final String DEFAULT_CODE = "AlreadyExists";
+    private static final String DEFAULT_CODE = "ALREADY_EXISTS";
 
     public AppObjectAlreadyExistsException(String code, String message) {
         super(code + DEFAULT_CODE ,message);

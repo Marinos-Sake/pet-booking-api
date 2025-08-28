7ae511df24ccef071f9d8436f5a4c51f34d058bc
Marinos.Sake <protutor13@hotmail.com>
2025-08-28 19:28:03 +0300

    refactor(exception): standardize DEFAULT_CODE naming to VALIDATION

--- src/main/java/com/petbooking/bookingapp/core/exception/AppValidationException.java
+++ src/main/java/com/petbooking/bookingapp/core/exception/AppValidationException.java
@@ -2,7 +2,7 @@ package com.petbooking.bookingapp.core.exception;
 
 
 public class AppValidationException extends AppGenericException {
-    private static final String DEFAULT_CODE = "Validation";
+    private static final String DEFAULT_CODE = "VALIDATION";
 
 
     public AppValidationException(String code, String message) {

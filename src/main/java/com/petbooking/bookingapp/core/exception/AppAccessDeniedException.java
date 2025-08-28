b68db6a3e83b82bb47d3d79bca82e3733150deb2
Marinos.Sake <protutor13@hotmail.com>
2025-08-28 19:26:28 +0300

    refactor(exception): standardize DEFAULT_CODE naming to ACCESS_DENIED

--- src/main/java/com/petbooking/bookingapp/core/exception/AppAccessDeniedException.java
+++ src/main/java/com/petbooking/bookingapp/core/exception/AppAccessDeniedException.java
@@ -2,7 +2,7 @@ package com.petbooking.bookingapp.core.exception;
 
 
 public class AppAccessDeniedException extends AppGenericException {
-    private static final String DEFAULT_CODE = "AccessDenied";
+    private static final String DEFAULT_CODE = "ACCESS_DENIED";
 
     public AppAccessDeniedException(String code, String message) {
         super(code + DEFAULT_CODE, message);

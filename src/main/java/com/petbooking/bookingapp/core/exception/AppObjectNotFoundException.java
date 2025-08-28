ca5ff4b19e8795ceb24af4e484f76eef74f85093
Marinos.Sake <protutor13@hotmail.com>
2025-08-28 19:27:47 +0300

    refactor(exception): standardize DEFAULT_CODE naming to NOT_FOUND

--- src/main/java/com/petbooking/bookingapp/core/exception/AppObjectNotFoundException.java
+++ src/main/java/com/petbooking/bookingapp/core/exception/AppObjectNotFoundException.java
@@ -3,7 +3,7 @@ package com.petbooking.bookingapp.core.exception;
 
 public class AppObjectNotFoundException extends AppGenericException{
 
-    private static final String DEFAULT_CODE = "NotFound";
+    private static final String DEFAULT_CODE = "NOT_FOUND";
 
     public AppObjectNotFoundException(String code, String message) {
         super(code + DEFAULT_CODE, message);

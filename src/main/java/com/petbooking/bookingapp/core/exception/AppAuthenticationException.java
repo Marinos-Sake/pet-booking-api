e77ab6dc47b97514710ff0b9923247502801d399
Marinos.Sake <protutor13@hotmail.com>
2025-08-28 19:26:44 +0300

    refactor(exception): standardize DEFAULT_CODE naming to AUTHENTICATION

--- src/main/java/com/petbooking/bookingapp/core/exception/AppAuthenticationException.java
+++ src/main/java/com/petbooking/bookingapp/core/exception/AppAuthenticationException.java
@@ -2,7 +2,7 @@ package com.petbooking.bookingapp.core.exception;
 
 
 public class AppAuthenticationException extends AppGenericException {
-    private static final String DEFAULT_CODE = "Authentication";
+    private static final String DEFAULT_CODE = "AUTHENTICATION";
 
     public AppAuthenticationException(String code, String message) {
         super(code + DEFAULT_CODE, message);

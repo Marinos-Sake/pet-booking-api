35f59c8918dd385591de2b6a044a8ff60701284d
Marinos.Sake <protutor13@hotmail.com>
2025-08-28 19:27:09 +0300

    refactor(exception): standardize DEFAULT_CODE naming to INVALID_STATE

--- src/main/java/com/petbooking/bookingapp/core/exception/AppInvalidStateException.java
+++ src/main/java/com/petbooking/bookingapp/core/exception/AppInvalidStateException.java
@@ -3,7 +3,7 @@ package com.petbooking.bookingapp.core.exception;
 
 public class AppInvalidStateException extends AppGenericException{
 
-    private static final String DEFAULT_CODE = "InvalidState";
+    private static final String DEFAULT_CODE = "INVALID_STATE";
 
     public AppInvalidStateException(String code, String message) {
         super(code+ DEFAULT_CODE, message);

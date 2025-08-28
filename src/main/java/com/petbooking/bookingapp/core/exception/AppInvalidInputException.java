14917c299c83d0bb9f53995547128680bac883b4
Marinos.Sake <protutor13@hotmail.com>
2025-08-28 19:26:57 +0300

    refactor(exception): standardize DEFAULT_CODE naming to INVALID_INPUT

--- src/main/java/com/petbooking/bookingapp/core/exception/AppInvalidInputException.java
+++ src/main/java/com/petbooking/bookingapp/core/exception/AppInvalidInputException.java
@@ -1,7 +1,7 @@
 package com.petbooking.bookingapp.core.exception;
 
 public class AppInvalidInputException extends AppGenericException{
-    private static final String DEFAULT_CODE = "InvalidInput";
+    private static final String DEFAULT_CODE = "INVALID_INPUT";
 
     public AppInvalidInputException(String code, String message) {
         super(code+ DEFAULT_CODE, message);

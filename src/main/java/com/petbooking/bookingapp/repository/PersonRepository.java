9825a782444d973dae2b86b81bfb0e247a50126c
Marinos.Sake <protutor13@hotmail.com>
2025-08-30 01:14:27 +0300

    feat(person-repository): add methods to check identity number existence

--- src/main/java/com/petbooking/bookingapp/repository/PersonRepository.java
+++ src/main/java/com/petbooking/bookingapp/repository/PersonRepository.java
@@ -4,4 +4,9 @@ import com.petbooking.bookingapp.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    boolean existsByIdentityNumber(String identityNumber);
+
+    boolean existsByIdentityNumberAndIdNot(String identityNumber, Long id);
+
 }

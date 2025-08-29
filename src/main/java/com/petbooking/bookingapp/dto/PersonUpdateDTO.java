9723ccda1f72e41e11ad730ff40f4e0fbdce814b
Marinos.Sake <protutor13@hotmail.com>
2025-08-30 01:15:33 +0300

    feat(person-update-dto): add field validations (NullOrNotBlank) to person data

--- src/main/java/com/petbooking/bookingapp/dto/PersonUpdateDTO.java
+++ src/main/java/com/petbooking/bookingapp/dto/PersonUpdateDTO.java
@@ -2,6 +2,7 @@ package com.petbooking.bookingapp.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.petbooking.bookingapp.core.enums.Gender;
+import com.petbooking.bookingapp.core.validation.NullOrNotBlank;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,18 +13,23 @@ import java.time.LocalDate;
 @Setter
 public class PersonUpdateDTO {
 
+    @NullOrNotBlank(message = "Name cannot be blank")
     private String name;
 
+    @NullOrNotBlank(message = "Surname cannot be blank")
     private String surname;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @PastOrPresent(message = "The date of birth cannot be in the future.")
     private LocalDate dateOfBirth;
 
+    @NullOrNotBlank(message = "Place of Birth cannot be blank")
     private String placeOfBirth;
 
+    @NullOrNotBlank(message = "Father name cannot be blank")
     private String fatherName;
 
+    @NullOrNotBlank(message = "Identity number cannot be blank")
     private String identityNumber;
 
     private Gender gender;

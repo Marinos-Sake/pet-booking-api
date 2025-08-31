21e10407b1d3b1604ec9013732ab8ef65e97f100
Marinos.Sake <protutor13@hotmail.com>
2025-08-31 23:49:16 +0300

    refactor(dto): rename BookingQuoteRequest to BookingQuoteRequestDTO

--- src/main/java/com/petbooking/bookingapp/dto/BookingQuoteRequestDTO.java
+++ src/main/java/com/petbooking/bookingapp/dto/BookingQuoteRequestDTO.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
-public class BookingQuoteRequest {
+public class BookingQuoteRequestDTO {
     private LocalDate checkInDate;
     private LocalDate checkOutDate;
     private Long roomId;

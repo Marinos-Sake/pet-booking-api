2475e30a523d8d6b1c1224025de4361c9b422925
Marinos.Sake <protutor13@hotmail.com>
2025-08-31 23:40:51 +0300

    feat(mapper): add mapToAttachmentEntity method in AttachmentMapper

--- src/main/java/com/petbooking/bookingapp/mapper/AttachmentMapper.java
+++ src/main/java/com/petbooking/bookingapp/mapper/AttachmentMapper.java
@@ -1,5 +1,6 @@
 package com.petbooking.bookingapp.mapper;
 
+import com.petbooking.bookingapp.service.internal.storage.AttachmentStorageInfo;
 import com.petbooking.bookingapp.dto.AttachmentReadOnlyDTO;
 import com.petbooking.bookingapp.entity.Attachment;
 import org.springframework.stereotype.Component;
@@ -15,7 +16,19 @@ public class AttachmentMapper {
         dto.setFileName(attachment.getFileName());
         dto.setContentType(attachment.getContentType());
         dto.setExtension(attachment.getExtension());
-
         return dto;
     }
+
+    public Attachment mapToAttachmentEntity(AttachmentStorageInfo storageInfo) {
+        if (storageInfo == null) return null;
+
+        Attachment attachment = new Attachment();
+        attachment.setFileName(storageInfo.getFileName());
+        attachment.setSavedName(storageInfo.getSavedName());
+        attachment.setFilePath(storageInfo.getFilePath());
+        attachment.setContentType(storageInfo.getContentType());
+        attachment.setExtension(storageInfo.getExtension());
+        return attachment;
+    }
+
 }

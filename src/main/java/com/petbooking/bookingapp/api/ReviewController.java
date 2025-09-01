0000f40ea76ae7b5127d54bf051323771a097d87
Marinos.Sake <protutor13@hotmail.com>
2025-09-01 16:01:20 +0300

    docs(review): add Swagger annotations for ReviewController endpoints

--- src/main/java/com/petbooking/bookingapp/api/ReviewController.java
+++ src/main/java/com/petbooking/bookingapp/api/ReviewController.java
@@ -4,6 +4,8 @@ import com.petbooking.bookingapp.dto.ReviewInsertDTO;
 import com.petbooking.bookingapp.dto.ReviewReadOnlyDTO;
 import com.petbooking.bookingapp.entity.User;
 import com.petbooking.bookingapp.service.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Review", description = "Endpoints for managing users reviews")
 @RestController
 @RequestMapping("/api/reviews")
 @RequiredArgsConstructor
@@ -21,25 +24,35 @@ public class ReviewController {
     private final ReviewService reviewService;
 
 
-    @PreAuthorize("hasRole('USER')")
+    @Operation(
+            summary = "Create a new review",
+            description = "Creates a new review for a specific booking by the authenticated user Access restricted to USER role"
+    )
     @PostMapping
     public ResponseEntity<ReviewReadOnlyDTO> createReview(
             @Valid @RequestBody ReviewInsertDTO dto,
             @RequestParam Long bookingId,
             @AuthenticationPrincipal User user
     ) {
-        ReviewReadOnlyDTO created = reviewService.createReview(dto, bookingId, user.getId());
+        ReviewReadOnlyDTO created = reviewService.createReview(dto, bookingId, user);
         return ResponseEntity.ok(created);
     }
 
 
+    @Operation(
+            summary = "[PUBLIC] Get all reviews",
+            description = "Retrieves a list of all reviews in the system"
+    )
     @GetMapping
     public ResponseEntity<List<ReviewReadOnlyDTO>> getAll() {
         return ResponseEntity.ok(reviewService.getAllReviews());
     }
 
 
-    @PreAuthorize("hasRole('USER')")
+    @Operation(
+            summary = "Get my reviews",
+            description = "Retrieves all reviews created by the currently authenticated user Accessible only to USER role"
+    )
     @GetMapping("/my")
     public ResponseEntity<List<ReviewReadOnlyDTO>> getMyReviews(@AuthenticationPrincipal User user) {
         return ResponseEntity.ok(reviewService.getReviewsByUserId(user.getId()));

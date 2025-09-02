e591d01ecbe93d6d0b094d00d3612e3d996814f5
Marinos.Sake <protutor13@hotmail.com>
2025-09-02 14:19:18 +0300

    feat(review): return 201 Created on successful review

--- src/main/java/com/petbooking/bookingapp/api/ReviewController.java
+++ src/main/java/com/petbooking/bookingapp/api/ReviewController.java
@@ -8,8 +8,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,7 +35,7 @@ public class ReviewController {
             @AuthenticationPrincipal User user
     ) {
         ReviewReadOnlyDTO created = reviewService.createReview(dto, bookingId, user);
-        return ResponseEntity.ok(created);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
 
